112bc9bd031c5dcf887ba2a38436ebb4770b9839
Kris <kristopher_hughes@yahoo.com>
2016-01-16 19:30:53 -0800

    added more functions for writehead need to fix json one

--- test/server.js
+++ test/server.js
@@ -8,12 +8,29 @@ var con = new Constructor();
 var server = http.createServer((req,res) => {
   req.requrl = url.parse(req.url, true);
   var path = req.requrl.pathname;
+
+//testing con.writeHeadHTML
   if (req.method === 'GET' && (path === '/' || path ==='/home')) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-  	var greeting = '<h1>Hello World</h1>';
+    //res.writeHead(200, {'Content-Type': 'text/html'});
+  	con.writeHeadHTML(res);
+    var greeting = '<h1>Hello World</h1>';
     res.write(greeting);
     return res.end();
-
+//testing con.writeHeadPLAIN
+  } else if (req.method === 'GET' && (path === '/plain')) {
+    //res.writeHead(200, {'Content-Type': 'text/html'});
+    con.writeHeadPLAIN(res);
+    var greeting = 'Hello World';
+    res.write(greeting);
+    return res.end();
+//testing con.writeHeadJSON
+  } else if (req.method === 'GET' && (path === '/json')) {
+    //res.writeHead(200, {'Content-Type': 'text/html'});
+    con.writeHeadJSON(res);
+    var greeting = "{'msg': 'Hello World'}";
+    res.write(JSON.stringify(greeting));
+    return res.end();
+//testing four04
   } else {
       con.four04(res);
       //con.logger;
