54889145577b1edc212b2e8ab6c70288ba905d7c
Rob Merrill <robgmerrill@gmail.com>
2016-01-18 16:28:02 -0800

    deleted extra comments non neccesary code

--- test/server.js
+++ test/server.js
@@ -1,4 +1,3 @@
-//const http = require('http');
 const Constructor = require(__dirname + '/../lib/frame');
 const http = require('http');
 const url = require('url');
@@ -11,26 +10,23 @@ var server = http.createServer((req,res) => {
   var path = req.requrl.pathname;
   if (req.method === 'GET' && (path === '/' || path ==='/home')) {
     con.writeHeadHTML(res);
-    //res.writeHead(200, {'Content-Type': 'text/html'});
     var html = fs.readFileSync(__dirname + '/../lib/index.html', 'utf8');
     res.write(html);
     return res.end();
 
   } else if (req.method === 'GET' && (path === '/plain')) {
-   //res.writeHead(200, {'Content-Type': 'text/html'});
-   con.writeHeadPLAIN(res);
-   var greeting = 'Hello World';
-   res.write(greeting);
-   return res.end();
+     con.writeHeadPLAIN(res);
+     var greeting = 'Hello World';
+     res.write(greeting);
+     return res.end();
 
   } else if (req.method === 'GET' && (path === '/json')) {
-   //res.writeHead(200, {'Content-Type': 'text/html'});
-   con.writeHeadJSON(res);
-   res.write(JSON.stringify({'msg': 'Hello World'}));
-   return res.end();
+     con.writeHeadJSON(res);
+     res.write(JSON.stringify({'msg': 'Hello World'}));
+     return res.end();
 
   } else {
-      con.four04(res);
+      con.four04(res);  //<--THE MAIN EVENT !!
   }
 }).listen(3000, () => { console.log('server started');});
 
